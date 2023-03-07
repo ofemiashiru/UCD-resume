5d500cdea285a4459d817405fd8542dcf206147d
ofemiashiru <ofemiashiru@gmail.com>
2023-03-07 11:46:15 +0000

    complete map challenge

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,28 @@
-let mapHolder = document.querySelector('#map')
 
-let APIKey = 'AIzaSyAii-CTEYkWIBCrGgwHx12QxIFxg79XbnA'
+function initMap(){
+    var  map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.13476
+        }
+    });
 
-let  map = new google.maps.Map(mapHolder)
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    var locations = [ 
+        {lat: 40.785091, lng: -73.968285},
+        {lat: 41.785091, lng: -75.968285},
+        {lat: 42.785091, lng: -79.968285}
+    ]
+
+    var markers = locations.map(function(location, i){
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        })
+    })
+
+    var markerCluster = new MarkerClusterer (map, markers,
+    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'})
+}
