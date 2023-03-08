66b427a83456992836550ff54e2998982460aa89
ofemiashiru <ofemiashiru@gmail.com>
2023-03-08 01:28:15 +0000

    multiple promises

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -19,40 +19,68 @@ const userInformationHTML = (user) =>{
 const fetchGitHubInformation = function(event){
 
     let username = document.querySelector('#gh-username').value
-    let userData = document.querySelector('#gh-user-data')
+    let userDataDisplay = document.querySelector('#gh-user-data')
 
     if(!username){
-        userData.innerHTML = '<h2>Please Enter a GitHub username</h2>'
+        userDataDisplay.innerHTML = '<h2>Please Enter a GitHub username</h2>'
         return;  
     }
 
     //using a animated gif to create a loader
-    document.querySelector('#gh-user-data').innerHTML = `
+    userDataDisplay.innerHTML = `
         <div id="loader">
             <img src="assets/css/loader.gif" alt="loading...">
         </div>
     `
+    //Using the Promis.all to get multiple Apis
+    Promise.all([
+        fetch(`https://api.github.com/users/${username}`),
+        fetch(`https://api.github.com/users/${username}/repos`)
+    ])
+    .then(responses =>{
 
-    fetch(`https://api.github.com/users/${username}`)
-    .then(res =>{
-
-        return res.json()
+        return Promise.all(responses.map(res => res.json()))
         
     })
     .then(data =>{
-        if(data.message === 'Not Found'){
+        let userData = data[0];
+        let userRepos = data[1];
 
-            userData.innerHTML = `<h2>No info found for user ${username}</h2>`
+        if(userData.message === 'Not Found'){
+
+            userDataDisplay.innerHTML = `<h2>No info found for user ${username}</h2>`
             
         } else {
 
-            userData.innerHTML = userInformationHTML(data)
+            userDataDisplay.innerHTML = userInformationHTML(userData)
         }
         
     })
     .catch((error)=>{
-        console.log('error', error.status)
+        console.log('error', error)
     })
+
+    /** fetch for a single item */
+    // fetch(`https://api.github.com/users/${username}`)
+    // .then(res =>{
+
+    //     return res.json()
+        
+    // })
+    // .then(data =>{
+    //     if(data.message === 'Not Found'){
+
+    //         userData.innerHTML = `<h2>No info found for user ${username}</h2>`
+            
+    //     } else {
+
+    //         userData.innerHTML = userInformationHTML(data)
+    //     }
+        
+    // })
+    // .catch((error)=>{
+    //     console.log('error', error.status)
+    // })
 
 
 }
